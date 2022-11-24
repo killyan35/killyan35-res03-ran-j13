f7ec23a413eea2fdcecd870040550a6882d90e72
killyan35 <kilyan.gerard@3wa.io>
2022-11-24 15:26:22 +0000

    impossible le js

--- main.js
+++ main.js
@@ -9,9 +9,61 @@ let section3 = document.getElementById("yellow");
 let section4 = document.getElementById("green");
 let section5 = document.getElementById("blue");
 let section6 = document.getElementById("purple");
-let position1 = section2.getBoundingClientRect().top;
+let position1 = section1.getBoundingClientRect().top;
+let position2 = section2.getBoundingClientRect().top;
+let position3 = section3.getBoundingClientRect().top;
+let position4 = section4.getBoundingClientRect().top;
+let position5 = section5.getBoundingClientRect().top;
+let position6 = section6.getBoundingClientRect().top;
 let sections =document.querySelectorAll("body > main >section");
-console.log(position1);
+
+function getNextSection(current)
+{
+    if (current.id==="red")
+    {
+        return document.getElementById("orange");
+    }
+    else if (current.id==="orange")
+    {
+        return document.getElementById("yellow");
+    }
+    else if (current.id==="yellow")
+    {
+        return document.getElementById("green");
+    }
+    else if (current.id==="green")
+    {
+        return document.getElementById("blue");
+    }
+    else if (current.id==="blue")
+    {
+        return document.getElementById("purple");
+    }
+}
+function getPreviewSection(current)
+{
+    if (current.id==="purple")
+    {
+        return document.getElementById("blue");
+    }
+    else if (current.id==="blue")
+    {
+        return document.getElementById("green");
+    }
+    else if (current.id==="green")
+    {
+        return document.getElementById("yellow");
+    }
+    else if (current.id==="yellow")
+    {
+        return document.getElementById("orange");
+    }
+    else if (current.id==="orange")
+    {
+        return document.getElementById("red");
+    }
+}
+
 for (let i=0;i<sections.length;i++) 
     {
         sections[i].addEventListener("wheel", function(event){
@@ -19,15 +71,17 @@ for (let i=0;i<sections.length;i++)
     if(event.deltaY > 0) // je vais vers le bas
     {
         let currentScrollPosition = document.documentElement.scrollTop;
-        let positionToGo = position1 ;
+        let positionToGo = getNextSection(currentScrollPosition+1) ;
         scroll(0, positionToGo);
     }
     else if(event.deltaY < 0) // je vais vers le haut
     {
+        let currentScrollPosition = document.documentElement.scrollTop;
+        let positionToGo = getPreviewSection(currentScrollPosition-1) ;
+        scroll(0, positionToGo);
+    }
     
-    }
-});
-    }
+});}
 
 
 
@@ -39,10 +93,4 @@ for (let i=0;i<sections.length;i++)
 
 
 
-function getNextSection(current)
-{
-    if (current.id==="red")
-    {
-        return section2
-    }
-}
+
